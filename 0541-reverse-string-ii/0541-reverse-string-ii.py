57bc44dcf0b8b643bfb636856d6949752e7c876e
Rachakonda Sai kumar <saikumarrachakonda305@gmail.com>
2025-06-23 12:04:37 +0530

    Time: 1 ms (48.92%), Space: 17.8 MB (90.09%) - LeetHub

--- 0541-reverse-string-ii/0541-reverse-string-ii.py
+++ 0541-reverse-string-ii/0541-reverse-string-ii.py
@@ -1,12 +1,17 @@
 class Solution:
     def reverseStr(self, s: str, k: int) -> str:
 
-        chunks = [s[i:i+k] for i in range(0, len(s), k)]
-        op = []
-        for i, chunk in enumerate(chunks):
-            if i%2 == 0:
-                chunk = chunk[::-1]
-            op.append(chunk)
-        # print(op)
-        return "".join(op)
+        # chunks = [s[i:i+k] for i in range(0, len(s), k)]
+        # op = []
+        # for i, chunk in enumerate(chunks):
+        #     if i%2 == 0:
+        #         chunk = chunk[::-1]
+        #     op.append(chunk)
+        # # print(op)
+        # return "".join(op)
+            s = list(s)
+            for i in range(0, len(s), 2 * k):
+                s[i:i + k] = reversed(s[i:i + k])
+            return "".join(s)
+
         
